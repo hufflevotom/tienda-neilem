8873cf59407cb918dd12df64d3f9893bac5f3095
Adrian <alescanoq@automa.edu.pe>
2023-06-25 16:26:43 -0500

    cambios header

--- src/layout/components/Header/Header.jsx
+++ src/layout/components/Header/Header.jsx
@@ -1,5 +1,23 @@
 const Header = () => {
-  return <div className="w-full h-14 text-lg">Header</div>;
+  return (
+    <header>
+      <div className="menu logo-nav">
+        <a href="Inicio" className="logo">
+          NEILEM SAC
+        </a>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <a href="nosotros">Nosotros</a>
+            </li>
+            <li>
+              <a href="contacto.html">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
