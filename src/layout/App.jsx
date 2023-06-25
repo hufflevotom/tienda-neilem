43e0acac7e7ba36271c0ecd249988196df846b8f
Bruno Breña Ramírez <ten.crypto@outlook.com>
2023-06-25 15:30:15 -0500

    feat: estructura

---
+++ src/layout/App.jsx
@@ -0,0 +1,15 @@
+import Main from "../routes/Main";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
