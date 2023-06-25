a764bbbb229a0d255e141a021abdcf63b0f7c523
Adrian <alescanoq@automa.edu.pe>
2023-06-25 16:54:11 -0500

    aea

--- src/routes/Nosotros/Nosotros.jsx
+++ src/routes/Nosotros/Nosotros.jsx
@@ -1,5 +1,27 @@
 const Nosotros = () => {
-  return <div>Nosotros</div>;
+  return (
+    <header>
+      <div className="menu logo-nav">
+        <a href="Nosotros" className="logo">
+          NEILEM SAC
+        </a>
+        <nav className="navigation">
+          <ul>
+            <li>
+              <a href="nosotros">Nosotros</a>
+            </li>
+            <li>
+              <a href="contacto.html">Contacto</a>
+            </li>
+            <li className="car">
+              <a href="carrito.html">
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Nosotros;
